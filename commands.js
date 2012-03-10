3bae55e807a03a9045491ee9fef8cd93643bf609
Hans Nieser <hans@nieser.nl>
2012-03-10 19:29:29 +0100

    add an actual youtube command

--- commands.js
+++ commands.js
@@ -143,46 +143,87 @@ commands["leavemsg"] =
     }
 }
 
-// Not really a 'command', more like a trigger.
+// Search for youtube videos, also prints titles for videos linked in a channel
 commands["youtube"] =
 {
+    // Helper function for querying the youtube API for movie info on given video ID or search query
+    fetchInfo: function (query, doSearch, resFunc)
+    {
+        var self = this;
+        var http = require("http");
+        var matches;
+        var options = { host: "gdata.youtube.com" };
+
+        if (doSearch)
+            options.path = "/feeds/api/videos?q="+encodeURIComponent(query)+"&max-results=1&alt=json&v=2";
+        else
+            options.path = "/feeds/api/videos/"+encodeURIComponent(query)+"?alt=json&v=2";
+
+        http.get(options, function (res)
+        {
+            var dataJSON = "";
+
+            res.on("data", function (data) { dataJSON += data; });
+            res.on("end", function ()
+            {
+                try
+                {
+                    var result = JSON.parse(dataJSON);
+
+                    if (doSearch)
+                        result = result.feed.entry[0];
+                    else
+                        result = result.entry;
+
+                    resFunc(result);
+                }
+                catch (e)
+                {
+                    self.log("failed to parse youtube JSON response: "+e.message);
+                    resFunc(null);
+                }
+            });
+        });
+    },
     hooks:
     {
         channelMessage: function (channel, sender, message)
         {
-            // Do a simple check before using full regex.
-            if (/youtu/.test(message))
+            var matches;
+            var self = this;
+
+            // If the message contained something that might be a youtube URL, pull out the movie ID
+            // and attempt to look it up.
+            if (/youtu/.test(message) &&
+                (matches = /(?:youtu.be\/|youtube\.com.*?[&\?]v=)([^ &#]+)/.exec(message)))
             {
-                var matches;
-                if (matches = /(?:youtu.be\/|youtube\.com.*?[&\?]v=)([^ &#]+)/.exec(message))
+                this.command.fetchInfo.call(this, matches[1], false, onInfo);
+
+                function onInfo(entry)
                 {
-                    var http = require("http");
-                    var self = this;
-
-                    var options = {
-                        host: "gdata.youtube.com",
-                        path: "/feeds/api/videos/"+encodeURIComponent(matches[1])+"?alt=json&v=2"
-                    };
-
-                    http.get(options, function (res)
-                    {
-                        var dataJSON = "";
-
-                        res.on("data", function (data) { dataJSON += data; });
-                        res.on("end", function ()
-                        {
-                            try
-                            {
-                                var result = JSON.parse(dataJSON);
-                                self.client.sendToChannel(channel, "title: "+result.entry.title.$t);
-                            }
-                            catch (e)
-                            {
-                                self.log("failed to parse youtube JSON response: "+e.message);
-                            }
-                        });
-                    });
+                    if (entry)
+                        self.client.sendToChannel(channel, "title: "+entry.title.$t);
+                    else
+                        self.log("channelMessage: failed to look up youtube movie");
                 }
+            }
+        }
+    },
+    params: "<query>",
+    description: "search youtube for given query string",
+    handler: function (query)
+    {
+        if (this.rawArgs)
+        {
+            var self = this;
+
+            this.command.fetchInfo.call(this, this.rawArgs, true, onInfo);
+
+            function onInfo(entry)
+            {
+                if (entry)
+                    self.reply("http://youtube.com/watch?v="+entry.media$group.yt$videoid.$t+
+                               " - "+entry.title.$t);
             }
         }
     }
